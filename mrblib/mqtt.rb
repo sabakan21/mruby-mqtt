60412cb13a1b8f9880f20bebca077e16b67cefec
sabakan21 <ax_nightmare@yahoo.co.jp>
2015-01-28 18:10:27 +0900

    defind Random class at init

--- mrblib/mqtt.rb
+++ mrblib/mqtt.rb
@@ -6,7 +6,7 @@ class MQTT < TCPSocket
 		attr_reader :client_id
 		def initialize(sockaddr, family=Socket::PF_UNSPEC, socktype=0, protocol=0)
 				@messageID = 1
-        @client_id = "mruby" + sprintf("%04d",rand(9999))
+        @client_id = "mruby" + sprintf("%04d",Random.rand(9999))
 				@time = Time.now
 				super
 		end
