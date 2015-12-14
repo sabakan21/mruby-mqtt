5454b47643619fc97e0fc8089c5553ce294cf7c9
FUJII Akira <fujii@sein.mse.kyutech.ac.jp>
2015-12-14 11:45:39 +0900

    add test

--- test/mqtt-unit-test.rb
+++ test/mqtt-unit-test.rb
@@ -1,3 +1,31 @@
-assert("mqtt#open") do
-  mq = MQTT.open("iot.eclipse.org", 1883)
+class Test4MQTT < MTest::Unit::TestCase
+  def mqttconnection
+    mq = MQTT.open("iot.eclipse.org", 1883)
+    mq.connect("mruby_test")
+    mq.recv 255
+    mq
+  end
+
+  def test_assert_open
+    assert("mqtt#open") do
+      mq = MQTT.open("iot.eclipse.org", 1883)
+    end
+  end
+
+  def test_assert_connect
+    mq = MQTT.open("iot.eclipse.org", 1883)
+    mq.connect("mruby_test")
+    res = mq.recv 255
+    assert_equal(" \002\000\000", res)
+  end
+
+  def test_assert_subscribe
+    mq = mqttconnection
+    mq.subscrb ("mruby/test")
+    res = mq.recv 255
+    assert_equal 
+  end
 end
+
+MTest::Unit.new.run
+
